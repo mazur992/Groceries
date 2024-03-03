0b9a435814256d9548b9d8153df6b35cc55c45e5
Mazur Roman <50218670+mazur992@users.noreply.github.com>
2024-03-03 11:07:14 +0100

    del unneces files

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button, Nav, Container, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { useShoppingCart } from "../context/ShoppingCartContext";
