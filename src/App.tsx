509033aac186e1e94261e1df179c4810d3758ab6
Mazur Roman <50218670+mazur992@users.noreply.github.com>
2024-03-03 14:28:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/store" element={<Store />} />
             <Route path="/about" element={<About />} />
-            <Route path="/*" element={<Home />} />
+            <Route path="*" element={<Home />} />
           </Routes>
         </Container>
       </ShoppingCartProvider>
